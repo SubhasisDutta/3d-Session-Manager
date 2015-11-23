e581dcd76f9b6eb4c54f7bdb9fe4e0fe86b63dc9
Subhasis Dutta <subhasisdutta300887@gmail.com>
2015-11-22 19:52:50 -0600

    Previous Progress - Web Application, Server and Client Socket

---
+++ 3dDatabaseManager/Controllers/SetupController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3dDatabaseManager.Controllers
+{
+    public class SetupController : Controller
+    {
+        //
+        // GET: /Setup/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ 3dDatabaseManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3dDatabaseManager.MvcApplication" Language="C#" %>
