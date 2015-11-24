ecf5e11c37ded67dd1089b26486a62347b39f6eb
Subhasis Dutta <subhasisdutta300887@gmail.com>
2015-11-23 20:39:04 -0600

    Include Async Server and Client Project

---
+++ 3dSessionManagerSolution/3DSessionListiningServer/MySQLManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace _3DSessionListiningServer
+{
+    public class MySQLManager
+    {
+        MySqlConnection conDatabase = null;
+        public MySQLManager()
+        {
+            string conststring = "Server=127.0.0.1;port=3306;Database=3dsessiondb;username=root;password=root;";
+            conDatabase = new MySqlConnection(conststring);
+            //conDatabase.Open();
+        }
+        public Boolean pushData(String message)
+        {
+            string query = "insert into messagelog (externalId,timestamp,dataType,data) VALUES ('TestingID','2015-01-01','TESTTYPE','" + message + "');";
+            MySqlCommand cmdDatabase = new MySqlCommand(query, this.conDatabase);
+            try
+            {
+                conDatabase.Open();
+                cmdDatabase.ExecuteReader();
+                //MySqlDataReader myReader = cmdDatabase.ExecuteReader();
+                Console.WriteLine("SAVED To DB" + message);
+                conDatabase.Close();
+                //while (myReader.Read())
+                //{
+
+                //}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return true;
+        }
+    }
+}
